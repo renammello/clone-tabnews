539a469925dde707e6940ce0d45dd943a13f78f6
Renam Mello da Silva <57508500+renammello@users.noreply.github.com>
2024-02-23 16:40:06 +0000

    adciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1> Gabrielly, você pode ser 🔥 às vezes, mas é sua risada 😂 e seu ❤️ enorme que me fazem te amar mais a cada dia.</h1>
+  return (
+    <h1>
+      {" "}
+      Gabrielly, você pode ser 🔥 às vezes, mas é sua risada 😂 e seu ❤️ enorme
+      que me fazem te amar mais a cada dia.
+    </h1>
+  );
 }
 
-export default Home;  
+export default Home;
